12a8837b3a10f39ce0ac831d64fb1a6db8ddc7c9
Aymaq <rashidaymaq1994@gmail.com>
2025-09-02 17:40:17 +0430

    fix: gh-pages path issue

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/coffee-app/", // 👈 add this line (repo name)
-  server: {
-    historyApiFallback: true,
-  },
+  base: "/coffee-app/", // 👈 repo name
 });
