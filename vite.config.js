4c41ad39093aef3d173c4d8e00e3d7716d4f419f
Aymaq <rashidaymaq1994@gmail.com>
2025-09-02 17:33:31 +0430

    fix: added base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/coffee-app/", // 👈 add this line (repo name)
   server: {
     historyApiFallback: true,
   },
